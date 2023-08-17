d35b86aed69e2e4460baa836f98c5a9b48466e93
HanGyeongjun <030212hgjun@gmail.com>
2023-08-17 23:37:40 +0900

    [Feat] Add WebView

--- Koreasy/Koreasy/ContentView.swift
+++ Koreasy/Koreasy/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .background(Color(UIColor.systemGray6))
+                .ignoresSafeArea(.all)
+            WebView(urlToLoad: "https://naver.com")
+                .edgesIgnoringSafeArea(.bottom)
         }
-        .padding()
     }
 }
 
