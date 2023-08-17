bde1e1c2d9085926ed8ae1224d57219219a8cf14
HanGyeongjun <030212hgjun@gmail.com>
2023-08-17 22:08:41 +0900

    [Init] Create project file

---
+++ Koreasy/Koreasy/KoreasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoreasyApp.swift
+//  Koreasy
+//
+//  Created by HanGyeongjun on 2023/08/17.
+//
+
+import SwiftUI
+
+@main
+struct KoreasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
